bed27bb74591bf45f10ea96f3ed315a6e6f1a1ed
Timothy <timothyolofson@gmail.com>
2025-01-22 03:13:18 -0800

    Practiced using reusable components.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@
 
 
 import './App.css';
-import Nav     from './components/Nav.jsx'
-import Landing from './components/Landing.jsx'
+import Nav        from './components/Nav.jsx';
+import Landing    from './components/Landing.jsx';
+import Highlights from './components/Highlights.jsx';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       <Nav />
       <Landing />
+      <Highlights />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars,faShoppingCart, faTimes);
+library.add(faBars,faShoppingCart, faTimes, faBolt, faBookOpen, faTags);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
