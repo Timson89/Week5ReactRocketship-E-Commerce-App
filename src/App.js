1f75e606df9194277a79aa4d491d2f1dd7d984ce
Timothy <timothyolofson@gmail.com>
2025-01-22 01:51:16 -0800

    Finished setting up envirement then started working on the navigation.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// Timothy Olofson
+// Frontend Simplified(FES)
+// Week 5 - React Rocketship - E-Commerce-App
+// January 22nd 2025
+
+
+import './App.css';
+import Nav from './components/Nav.jsx'
+
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import LibraryLogo from '../assets/Library.svg'; 
+
+
+const Nav = () => {
+
+  return (
+
+    <nav>
+      <div className="nav__container">
+        <a href="/">
+          <img className="logo" src={LibraryLogo} alt="" />
+        </a>
+        <ul className="nav__links">
+          <li className="nav__list">
+            <a className="nav__link" href="/">
+        Home
+            </a>
+          </li>
+          <li className="nav__list">
+            <a className="nav__link" href="/book">
+        Book
+            </a>
+          </li>
+          <button className="btn__menu">
+            <FontAwesomeIcon icon={'bars'} />
+          </button>
+          <li className="nav__icon">
+            <a className="nav__link" href="/cart">
+              <FontAwesomeIcon icon={'shopping-cart'} />
+            </a>
+            <span className="cart__length">2</span>
+          </li>
+        </ul>
+        <div className="menu__backdrop">
+          <button className="btn__menu btn__menu--close">
+            <FontAwesomeIcon icon={'times'} />
+          </button>
+          <ul className="menu__links">
+            <li className="menu__list">
+              <a className="menu__link" href="/">
+          Home
+              </a>
+            </li>
+            <li className="menu__list">
+              <a className="menu__link" href="/book">
+          Book
+              </a>
+            </li>
+            <li className="menu__list">
+              <a className="menu__link" href="/cart">
+          Cart
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
